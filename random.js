7efb1aebfe84add5bf6db3102a0194d93cb11cd4
Kalyn <kalyneisenbeis@u.boisestate.edu>
2022-03-24 21:22:26 -0600

    random changes

--- random.js
+++ random.js
@@ -4,3 +4,10 @@ let backgroundChange = document.querySelector("#elementChange");
 backgroundChange.addEventListener("click", () =>{
     document.body.style.backgroundImage = "url('images/background.jpg')";
 });
+
+//Function that changes css property when button is clicked
+let btn2 = document.querySelector("#cssChange");
+
+btn2.addEventListener("click", ()=>{
+    
+})
