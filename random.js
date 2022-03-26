4201db9c748ffd4af45ea478915920cacd768648
Evanthiede <ethiede17@gmail.com>
2022-03-26 16:11:59 -0600

    Balloon Function
    made a function that adds a number of balloons that is the same as the random number generator

--- random.js
+++ random.js
@@ -20,18 +20,49 @@ function myFunction() {
     document.getElementById("output").innerHTML = x;
   }
   
-function randomFunction() {
+
+var finalNum;
+
+
+function randomFunction() 
+{
     var maxNum = document.getElementById("maxBox").value;
     var minNum = document.getElementById("minBox").value;
-    if(minNum < maxNum)
-        {
-            var finalNum = getRandomIntInclusive(minNum,maxNum)
-        }
-    document.getElementById("numOutput").innerHTML = finalNum;
-  }
 
+    //inserts range into random number function
+    finalNum = getRandomIntInclusive(minNum,maxNum)
+    console.log(finalNum)
+        
+    //displays random number
+    document.getElementById("numOutput").innerHTML = finalNum;
+}
+
+//makes random number from ranges given above
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
   }
+
+function createBalloons(){
+
+    document.getElementById("balloonId").innerHTML = "";
+
+    for(i = 0; i < finalNum; i++)
+    {
+        
+        let divElement = document.createElement('div')
+
+        // create text node
+        let divElementText = document.createTextNode('BALLOON')
+
+        // append text node to div
+        divElement.appendChild(divElementText)
+
+        let balloons = document.querySelector('#balloonId')
+
+        // append div element to document
+        balloons.appendChild(divElement)
+
+    }
+}
